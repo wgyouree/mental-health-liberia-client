528afbae1de67a14bc70b19c8c0bc653fb7a82d0
wgyouree <wgyouree@gmail.com>
2012-03-04 00:07:16 +0000

    initial commit

---
+++ src/mentalhealthliberia/DataStore.java
@@ -0,0 +1,206 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mentalhealthliberia;
+
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ *
+ * @author greg
+ */
+public class DataStore {
+    
+    public enum FileStatus {
+        READY, OUTOFDATE, ERROR
+    }
+    
+    private static final String FORM_DIR = "forms";
+    
+    private static DataStore INSTANCE = new DataStore();
+    
+    private DataStore() {}
+    
+    public static DataStore getInstance() {
+        return INSTANCE;
+    }
+    
+    public FileStatus sendFile(File file, String username, String password) {
+        try {
+            // Construct data
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String data = "";
+            String curr = reader.readLine();
+            while (curr != null) {
+                data += curr;
+                curr = reader.readLine();
+            }
+
+            // append password field
+
+            reader.close();
+
+            String charset = "UTF-8";
+            String query = String.format("username=%s&password=%s&data=%s", URLEncoder.encode(username, charset), 
+                    URLEncoder.encode(password, charset), URLEncoder.encode(data, charset));
+            System.out.println(query);
+
+            try {
+                boolean versionOutOfDate = false;
+
+                // Send data
+                URL url = new URL(MentalHealthLiberiaApp.getApplication().getUploadUrl());
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(query);
+                wr.flush();
+
+                // Get the response
+                if (conn.getResponseCode() != 200) {
+                    wr.close();
+                    return FileStatus.READY;
+                } else {
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        Integer version = Integer.parseInt(line);
+                        if (version > MentalHealthLiberiaApp.getApplication().getVersionNum()) {
+                            // version is out of date
+                            versionOutOfDate = true;
+                        }
+                    }
+                    rd.close();
+                }
+
+                wr.close();
+
+                if (versionOutOfDate) {
+                    return FileStatus.OUTOFDATE;
+                }
+
+                return FileStatus.READY;
+
+            } catch (Exception e) {
+                System.err.println("Error occured while transmitting data.\n" +
+                        "Please contact a system administrator.");
+                System.err.println(e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.err.println("Error occured while reading saved form data.");
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return FileStatus.ERROR;
+    }
+
+    public boolean saveForm(PatientEncounterForm form) {
+        String dataDir = MentalHealthLiberiaApp.getApplication().getDataDirectory();
+        File directory = new File(dataDir + FORM_DIR);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        File newFile = null;
+        if (directory.isDirectory()) {
+            try {
+                String fileName = form.getClinicianID() + '-' + form.getDateOfService() + ".mhl";
+                newFile = new File(directory.getAbsolutePath() + "/" + removeInvalidCharacters(fileName));
+                newFile.createNewFile();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
+                JSONSerializer serializer = new JSONSerializer();
+                String data = serializer.serialize(form);
+                writer.write(data);
+                writer.close();
+                return true;
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+                if (newFile != null) {
+                    newFile.delete();
+                }
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    
+    private String removeInvalidCharacters(String fileName) {
+        try {
+            return URLEncoder.encode(fileName.trim(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return fileName.trim();
+        }
+    }
+    
+    public List<PatientEncounterForm> getPendingForms() {
+        List<PatientEncounterForm> forms = new ArrayList<PatientEncounterForm>();
+        File directory = new File(MentalHealthLiberiaApp.getApplication().getDataDirectory() + FORM_DIR);
+        try {
+            for (File file : directory.listFiles()) {
+                String jsonString = "";
+                StringBuilder fileData = new StringBuilder(1000);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                char[] buf = new char[1024];
+                int numRead=0;
+                while((numRead=reader.read(buf)) != -1){
+                    fileData.append(buf, 0, numRead);
+                }
+                reader.close();
+                jsonString = fileData.toString();
+
+                PatientEncounterForm form = new JSONDeserializer<PatientEncounterForm>()
+                        .deserialize(jsonString);
+
+                forms.add(form);
+            }
+        } catch (Exception e) {
+            System.err.println("File does not appear to be valid MHL file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return forms;
+    }
+    
+    public FileStatus uploadFiles(String username, String password) {
+        boolean errorEncountered = false;
+        File directory = new File(MentalHealthLiberiaApp.getApplication().getDataDirectory() + FORM_DIR);
+        File[] files = directory.listFiles();
+        for (int i = 0; i < files.length; i++) {
+
+            // upload pending file
+            if (sendFile(files[i], username, password) != DataStore.FileStatus.READY) {
+                // delete pending file
+                files[i].delete();
+            } else {
+                errorEncountered = true;
+                break;
+            }
+        }
+
+        // notify user of result
+        if (errorEncountered) {
+            return FileStatus.ERROR;
+
+        } else {
+            return FileStatus.READY;
+        }
+    }
+}
---
+++ src/mentalhealthliberia/PatientEncounterForm.java
@@ -0,0 +1,641 @@
+/*
+ *
+ */
+package mentalhealthliberia;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * POJO which stores the patient encounter form data.
+ * 
+ * @author W. Greg Youree
+ */
+public class PatientEncounterForm implements Serializable {
+
+    public String getAge() {
+        return age;
+    }
+
+    public boolean isAmitriptyline() {
+        return amitriptyline;
+    }
+
+    public String getAnxietyDisorder() {
+        return anxietyDisorder;
+    }
+
+    public String getCage() {
+        return cage;
+    }
+
+    public boolean isCarbamazepine() {
+        return carbamazepine;
+    }
+
+    public boolean isCarbmazepineAntiepileptic() {
+        return carbmazepineAntiepileptic;
+    }
+
+    public boolean isChlorpromazine() {
+        return chlorpromazine;
+    }
+
+    public String getClinicianID() {
+        return clinicianID;
+    }
+
+    public String getClinicianTrainingLevel() {
+        return clinicianTrainingLevel;
+    }
+
+    public boolean isClomipramine() {
+        return clomipramine;
+    }
+
+    public boolean isClonazepam() {
+        return clonazepam;
+    }
+
+    public String getCounseling() {
+        return counseling;
+    }
+
+    public String getCountyOfResidence() {
+        return countyOfResidence;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Date getDateOfService() {
+        return dateOfService;
+    }
+
+    public boolean isDepakote() {
+        return depakote;
+    }
+
+    public boolean isDepakoteAntiepileptic() {
+        return depakoteAntiepileptic;
+    }
+
+    public String getDiagnosisPrimary() {
+        return diagnosisPrimary;
+    }
+
+    public boolean isDiazepam() {
+        return diazepam;
+    }
+
+    public String getDischargeDisposition() {
+        return dischargeDisposition;
+    }
+
+    public String getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public String getEmploymentStatus() {
+        return employmentStatus;
+    }
+
+    public String getEpilepsy() {
+        return epilepsy;
+    }
+
+    public boolean isEscitalopram() {
+        return escitalopram;
+    }
+
+    public String getFamilyPsychoEducation() {
+        return familyPsychoEducation;
+    }
+
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public boolean isFluoxetine() {
+        return fluoxetine;
+    }
+
+    public boolean isFluphenazine() {
+        return fluphenazine;
+    }
+
+    public boolean isFluphenazineDecanoateInjection() {
+        return fluphenazineDecanoateInjection;
+    }
+
+    public boolean isFollowUpCareCounseling() {
+        return followUpCareCounseling;
+    }
+
+    public boolean isFollowUpCareMedication() {
+        return followUpCareMedication;
+    }
+
+    public String getGaf() {
+        return gaf;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public boolean isHaloperidal() {
+        return haloperidal;
+    }
+
+    public boolean isHaloperidalDecanoateInjection() {
+        return haloperidalDecanoateInjection;
+    }
+
+    public boolean isImipramine() {
+        return imipramine;
+    }
+
+    public String getIndividualCounseling() {
+        return individualCounseling;
+    }
+
+    public boolean isLithium() {
+        return lithium;
+    }
+
+    public String getLocationOfService() {
+        return locationOfService;
+    }
+
+    public boolean isLorazepam() {
+        return lorazepam;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public String getMoodDisorder() {
+        return moodDisorder;
+    }
+
+    public boolean isOtherAnticholinergic() {
+        return otherAnticholinergic;
+    }
+
+    public boolean isOtherAntidepressant() {
+        return otherAntidepressant;
+    }
+
+    public boolean isOtherAntiepileptic() {
+        return otherAntiepileptic;
+    }
+
+    public boolean isOtherAntipsychotic() {
+        return otherAntipsychotic;
+    }
+
+    public String getOtherMedicalCondition() {
+        return otherMedicalCondition;
+    }
+
+    public boolean isOtherMoodStabilizer() {
+        return otherMoodStabilizer;
+    }
+
+    public boolean isOtherSedative() {
+        return otherSedative;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public boolean isPhenobarbital() {
+        return phenobarbital;
+    }
+
+    public boolean isPhenytoin() {
+        return phenytoin;
+    }
+
+    public String getPhq() {
+        return phq;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public String getPsychoticDisorder() {
+        return psychoticDisorder;
+    }
+
+    public String getReasonForVisit() {
+        return reasonForVisit;
+    }
+
+    public String getReferralSource() {
+        return referralSource;
+    }
+
+    public boolean isReportData() {
+        return reportData;
+    }
+
+    public boolean isRisperidone() {
+        return risperidone;
+    }
+
+    public boolean isRisperidoneConstaInjection() {
+        return risperidoneConstaInjection;
+    }
+
+    public String getSecondaryDiagnosis() {
+        return secondaryDiagnosis;
+    }
+
+    public boolean isSertraline() {
+        return sertraline;
+    }
+
+    public String getSomatoformDisorder() {
+        return somatoformDisorder;
+    }
+
+    public String getSubstanceAbuseDisorder() {
+        return substanceAbuseDisorder;
+    }
+
+    public String getSubstanceAbuseDisorder2() {
+        return substanceAbuseDisorder2;
+    }
+
+    public boolean isTrihexyphenidyl() {
+        return trihexyphenidyl;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setAmitriptyline(boolean amitriptyline) {
+        this.amitriptyline = amitriptyline;
+    }
+
+    public void setAnxietyDisorder(String anxietyDisorder) {
+        this.anxietyDisorder = anxietyDisorder;
+    }
+
+    public void setCage(String cage) {
+        this.cage = cage;
+    }
+
+    public void setCarbamazepine(boolean carbamazepine) {
+        this.carbamazepine = carbamazepine;
+    }
+
+    public void setCarbmazepineAntiepileptic(boolean carbmazepineAntiepileptic) {
+        this.carbmazepineAntiepileptic = carbmazepineAntiepileptic;
+    }
+
+    public void setChlorpromazine(boolean chlorpromazine) {
+        this.chlorpromazine = chlorpromazine;
+    }
+
+    public void setClinicianID(String clinicianID) {
+        this.clinicianID = clinicianID;
+    }
+
+    public void setClinicianTrainingLevel(String clinicianTrainingLevel) {
+        this.clinicianTrainingLevel = clinicianTrainingLevel;
+    }
+
+    public void setClomipramine(boolean clomipramine) {
+        this.clomipramine = clomipramine;
+    }
+
+    public void setClonazepam(boolean clonazepam) {
+        this.clonazepam = clonazepam;
+    }
+
+    public void setCounseling(String counseling) {
+        this.counseling = counseling;
+    }
+
+    public void setCountyOfResidence(String countyOfResidence) {
+        this.countyOfResidence = countyOfResidence;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setDateOfService(Date dateOfService) {
+        this.dateOfService = dateOfService;
+    }
+
+    public void setDepakote(boolean depakote) {
+        this.depakote = depakote;
+    }
+
+    public void setDepakoteAntiepileptic(boolean depakoteAntiepileptic) {
+        this.depakoteAntiepileptic = depakoteAntiepileptic;
+    }
+
+    public void setDiagnosisPrimary(String diagnosisPrimary) {
+        this.diagnosisPrimary = diagnosisPrimary;
+    }
+
+    public void setDiazepam(boolean diazepam) {
+        this.diazepam = diazepam;
+    }
+
+    public void setDischargeDisposition(String dischargeDisposition) {
+        this.dischargeDisposition = dischargeDisposition;
+    }
+
+    public void setDistanceTraveled(String distanceTraveled) {
+        this.distanceTraveled = distanceTraveled;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public void setEmploymentStatus(String employmentStatus) {
+        this.employmentStatus = employmentStatus;
+    }
+
+    public void setEpilepsy(String epilepsy) {
+        this.epilepsy = epilepsy;
+    }
+
+    public void setEscitalopram(boolean escitalopram) {
+        this.escitalopram = escitalopram;
+    }
+
+    public void setFamilyPsychoEducation(String familyPsychoEducation) {
+        this.familyPsychoEducation = familyPsychoEducation;
+    }
+
+    public void setFathersName(String fathersName) {
+        this.fathersName = fathersName;
+    }
+
+    public void setFluoxetine(boolean fluoxetine) {
+        this.fluoxetine = fluoxetine;
+    }
+
+    public void setFluphenazine(boolean fluphenazine) {
+        this.fluphenazine = fluphenazine;
+    }
+
+    public void setFluphenazineDecanoateInjection(boolean fluphenazineDecanoateInjection) {
+        this.fluphenazineDecanoateInjection = fluphenazineDecanoateInjection;
+    }
+
+    public void setFollowUpCareCounseling(boolean followUpCareCounseling) {
+        this.followUpCareCounseling = followUpCareCounseling;
+    }
+
+    public void setFollowUpCareMedication(boolean followUpCareMedication) {
+        this.followUpCareMedication = followUpCareMedication;
+    }
+
+    public void setGaf(String gaf) {
+        this.gaf = gaf;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setHaloperidal(boolean haloperidal) {
+        this.haloperidal = haloperidal;
+    }
+
+    public void setHaloperidalDecanoateInjection(boolean haloperidalDecanoateInjection) {
+        this.haloperidalDecanoateInjection = haloperidalDecanoateInjection;
+    }
+
+    public void setImipramine(boolean imipramine) {
+        this.imipramine = imipramine;
+    }
+
+    public void setIndividualCounseling(String individualCounseling) {
+        this.individualCounseling = individualCounseling;
+    }
+
+    public void setLithium(boolean lithium) {
+        this.lithium = lithium;
+    }
+
+    public void setLocationOfService(String locationOfService) {
+        this.locationOfService = locationOfService;
+    }
+
+    public void setLorazepam(boolean lorazepam) {
+        this.lorazepam = lorazepam;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public void setMoodDisorder(String moodDisorder) {
+        this.moodDisorder = moodDisorder;
+    }
+
+    public void setOtherAnticholinergic(boolean otherAnticholinergic) {
+        this.otherAnticholinergic = otherAnticholinergic;
+    }
+
+    public void setOtherAntidepressant(boolean otherAntidepressant) {
+        this.otherAntidepressant = otherAntidepressant;
+    }
+
+    public void setOtherAntiepileptic(boolean otherAntiepileptic) {
+        this.otherAntiepileptic = otherAntiepileptic;
+    }
+
+    public void setOtherAntipsychotic(boolean otherAntipsychotic) {
+        this.otherAntipsychotic = otherAntipsychotic;
+    }
+
+    public void setOtherMedicalCondition(String otherMedicalCondition) {
+        this.otherMedicalCondition = otherMedicalCondition;
+    }
+
+    public void setOtherMoodStabilizer(boolean otherMoodStabilizer) {
+        this.otherMoodStabilizer = otherMoodStabilizer;
+    }
+
+    public void setOtherSedative(boolean otherSedative) {
+        this.otherSedative = otherSedative;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public void setPhenobarbital(boolean phenobarbital) {
+        this.phenobarbital = phenobarbital;
+    }
+
+    public void setPhenytoin(boolean phenytoin) {
+        this.phenytoin = phenytoin;
+    }
+
+    public void setPhq(String phq) {
+        this.phq = phq;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public void setPsychoticDisorder(String psychoticDisorder) {
+        this.psychoticDisorder = psychoticDisorder;
+    }
+
+    public void setReasonForVisit(String reasonForVisit) {
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public void setReferralSource(String referralSource) {
+        this.referralSource = referralSource;
+    }
+
+    public void setReportData(boolean reportData) {
+        this.reportData = reportData;
+    }
+
+    public void setRisperidone(boolean risperidone) {
+        this.risperidone = risperidone;
+    }
+
+    public void setRisperidoneConstaInjection(boolean risperidoneConstaInjection) {
+        this.risperidoneConstaInjection = risperidoneConstaInjection;
+    }
+
+    public void setSecondaryDiagnosis(String secondaryDiagnosis) {
+        this.secondaryDiagnosis = secondaryDiagnosis;
+    }
+
+    public void setSertraline(boolean sertraline) {
+        this.sertraline = sertraline;
+    }
+
+    public void setSomatoformDisorder(String somatoformDisorder) {
+        this.somatoformDisorder = somatoformDisorder;
+    }
+
+    public void setSubstanceAbuseDisorder(String substanceAbuseDisorder) {
+        this.substanceAbuseDisorder = substanceAbuseDisorder;
+    }
+
+    public void setSubstanceAbuseDisorder2(String substanceAbuseDisorder2) {
+        this.substanceAbuseDisorder2 = substanceAbuseDisorder2;
+    }
+
+    public void setTrihexyphenidyl(boolean trihexyphenidyl) {
+        this.trihexyphenidyl = trihexyphenidyl;
+    }
+    
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Clinician: " + getClinicianID() + " ");
+        builder.append("Date of Service: " + getDateOfService() + " ");
+        return builder.toString();
+    }
+    
+    // Clinician Info
+    private String clinicianID;
+    
+    // Basic Information
+    private Date dateOfService;
+    private String locationOfService;
+    private String reasonForVisit;
+    private String clinicianTrainingLevel;
+    private String referralSource;
+    
+    // Patient Demographics (used for generating unique ID)
+    private String patientName;
+    private String dateOfBirth;
+    private String gender;
+    private String fathersName;
+    private String placeOfBirth;
+    
+    // Patient Demographics
+    private String age;
+    private String countyOfResidence;
+    private String distanceTraveled;
+    private String employmentStatus;
+    private String maritalStatus;
+    private String education;
+    
+    // Symptoms and Functioning
+    private String phq;
+    private String gaf;
+    private String cage;
+    
+    // Diagnosis
+    private String diagnosisPrimary;
+    private String moodDisorder;
+    private String anxietyDisorder;
+    private String psychoticDisorder;
+    private String somatoformDisorder;
+    private String substanceAbuseDisorder;
+    private String substanceAbuseDisorder2;
+    private String epilepsy;
+    private String otherMedicalCondition;
+    private String secondaryDiagnosis;
+    
+    // Treatment
+    private boolean fluoxetine;
+    private boolean escitalopram;
+    private boolean sertraline;
+    private boolean amitriptyline;
+    private boolean imipramine;
+    private boolean otherAntidepressant;
+    private boolean haloperidal;
+    private boolean haloperidalDecanoateInjection;
+    private boolean chlorpromazine;
+    private boolean fluphenazine;
+    private boolean fluphenazineDecanoateInjection;
+    private boolean risperidone;
+    private boolean risperidoneConstaInjection;
+    private boolean otherAntipsychotic;
+    private boolean clomipramine;
+    private boolean clonazepam;
+    private boolean diazepam;
+    private boolean lorazepam;
+    private boolean otherSedative;
+    private boolean depakote;
+    private boolean lithium;
+    private boolean carbamazepine;
+    private boolean otherMoodStabilizer;
+    private boolean depakoteAntiepileptic;
+    private boolean carbmazepineAntiepileptic;
+    private boolean phenobarbital;
+    private boolean phenytoin;
+    private boolean otherAntiepileptic;
+    private boolean trihexyphenidyl;
+    private boolean otherAnticholinergic;
+    private String counseling;
+    private String individualCounseling;
+    private String familyPsychoEducation;
+    
+    // Discharge
+    private boolean followUpCareMedication;
+    private boolean followUpCareCounseling;
+    private String dischargeDisposition;
+    private boolean reportData;
+}
